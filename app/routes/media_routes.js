056a513ca81e1143f9646958572fe55254de994c
Angeline <ammeitzler@gmail.com>
2018-09-12 11:50:52 -0400

    changed database

--- app/routes/media_routes.js
+++ app/routes/media_routes.js
@@ -3,7 +3,7 @@ module.exports = function(app, db) {
   var ObjectID = require('mongodb').ObjectID;
 
   app.get("/media", function(req, res, next) {
-    db.collection("media_data", function(err, result){
+    db.collection("video_data", function(err, result){
         result.find({}).sort({_id: -1}).limit(10).toArray(function(err, data){
           res.json(data);
       })
@@ -13,7 +13,7 @@ module.exports = function(app, db) {
   app.delete('/media/:id', (req, res) => {
     const id = req.params.id;
     const details = { '_id': new ObjectID(id) };
-    db.collection('media_data').remove(details, (err, item) => {
+    db.collection('video_data').remove(details, (err, item) => {
       if (err) {
         res.send({'error':'An error has occurred'});
       } else {
@@ -25,7 +25,7 @@ module.exports = function(app, db) {
   app.post('/media', (req, res) => {
     const media_data = { title: req.body.title, message: req.body.message };
 
-    db.collection('media_data').insert(media_data, (err, result) => {
+    db.collection('video_data').insert(media_data, (err, result) => {
       if (err) { 
         res.send({ 'error': 'An error has occurred' }); 
       } else {
